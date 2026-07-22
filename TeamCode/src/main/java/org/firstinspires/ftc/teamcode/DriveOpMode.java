e767e13fb309d3e2f6659971171e50651fae08fd
jibraan10 <jibraanahmed10@gmail.com>
2026-07-22 10:45:14 -0400

    added comments for live debugging
    switched data field variables to local
    
    fixed shooting motors toggle function - it used to
    be that they would continuously toggle (like hundreds of times a second) when you held the right bumper - changed to rising edge toggle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -6,28 +6,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp()
 public class DriveOpMode extends LinearOpMode {
-
-    private DcMotor frontleft;
-    private DcMotor frontright;
-    private DcMotor backleft;
-    private DcMotor backright;
-    private DcMotor intake;
-    private DcMotor shootright;
-    private DcMotor shootleft;
-    private DcMotor transfer;
-
     @Override
     public void runOpMode() {
-        frontleft = hardwareMap.get(DcMotor.class, "frontleft");
-        frontright = hardwareMap.get(DcMotor.class, "frontright");
-        backleft = hardwareMap.get(DcMotor.class, "backleft");
-        backright = hardwareMap.get(DcMotor.class, "backright");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        transfer = hardwareMap.get(DcMotor.class, "transfer");
-        shootleft = hardwareMap.get(DcMotor.class, "shootleft");
-        shootright = hardwareMap.get(DcMotor.class, "shootright");
+        DcMotor frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+        DcMotor frontright = hardwareMap.get(DcMotor.class, "frontright");
+        DcMotor backleft = hardwareMap.get(DcMotor.class, "backleft");
+        DcMotor backright = hardwareMap.get(DcMotor.class, "backright");
+        DcMotor intake = hardwareMap.get(DcMotor.class, "intake");
+        DcMotor transfer = hardwareMap.get(DcMotor.class, "transfer");
+        DcMotor shootleft = hardwareMap.get(DcMotor.class, "shootleft");
+        DcMotor shootright = hardwareMap.get(DcMotor.class, "shootright");
 
         boolean shooting = false;
+        boolean lastrightbumper = false;
 
         // reversing right motors
 //        frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -60,34 +51,41 @@ public class DriveOpMode extends LinearOpMode {
             // forward intake
             if (gamepad1.left_bumper) {
                 intake.setPower(1);
+                System.out.println("forward intake...");
             // reverse intake
             } else if (gamepad1.b) {
                 intake.setPower(-1);
-                transfer.setPower(-1);
+                transfer.setPower(-1); // don't need the setPower(0) line in the else statement here because in the next if block it already sets it to 0
+                System.out.println("reverse intake...");
             } else {
                 intake.setPower(0);
             }
 
             // TRANSFER + SHOOTING:
             // check if shooter should be stopped or started
-            if (gamepad1.right_bumper) {
-                // toggle off or on
-                shooting = !shooting;
-
-                // change motor powers according to new value of "shooting"
-                if (shooting) {
-                    shootright.setPower(0.5);
-                    shootleft.setPower(-0.5);
-                } else {
-                    shootright.setPower(0);
-                    shootleft.setPower(0);
-                }
-            }=
-            [
-                    'if (gamepad1.a) {
+            if (gamepad1.a) {
                 transfer.setPower(1);
+                System.out.println("shooting...");
             } else {
                 transfer.setPower(0);
+            }
+
+            if (gamepad1.right_bumper != lastrightbumper) {
+                // toggle off or on
+                shooting = !shooting;
+                System.out.println("shooting = " + shooting);
+            }
+
+            lastrightbumper = gamepad1.right_bumper;
+
+            // change motor powers according to new value of "shooting"
+            if (shooting) {
+                shootright.setPower(0.5);
+                shootleft.setPower(-0.5);
+            } else {
+                shootright.setPower(0);
+                shootleft.setPower(0);
+            }
             }
         }
     }
